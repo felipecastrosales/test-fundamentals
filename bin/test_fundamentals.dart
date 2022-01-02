2f58544b005a0e14c3c35b00da1a119048c699a2
Felipe Sales <soufeliposales@gmail.com>
2022-01-02 10:55:52 -0300

    initial commit

---
+++ bin/test_fundamentals.dart
@@ -0,0 +1,5 @@
+import 'package:test_fundamentals/test_fundamentals.dart' as test_fundamentals;
+
+void main(List<String> arguments) {
+  print('Hello world: ${test_fundamentals.calculate()}!');
+}
---
+++ test/test_fundamentals_test.dart
@@ -0,0 +1,8 @@
+import 'package:test_fundamentals/test_fundamentals.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
